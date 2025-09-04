83243a683c5530d39e55111f45bd5b1e1aee96ed
matisuarezm <mati.suarez@duocuc.cl>
2025-09-03 22:26:30 -0400

    Este es mi primer commit

---
+++ hellogit.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author msuarez
+ */
+public class hellogit {
+    public static void main(String[] args) {
+        System.out.println("Holla GitHub");        
+        
+    }    
+}
+
+
