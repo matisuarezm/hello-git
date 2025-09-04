6bde44e51ceaa5abcffd1e820b6becb0008e4d5f
matisuarezm <mati.suarez@duocuc.cl>
2025-09-03 22:32:24 -0400

    Este es mi segundo commit

---
+++ hellogit2.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author msuarez
+ */
+public class hellogit {
+    public static void main(String[] args) {
+        System.out.println("Holla GitHub");
+        System.out.println("Holla GitHub Ingresamos otros datos");
+        System.out.println("Esperamos no molestar....");
+
+    }
+}
