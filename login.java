1afa5fc4f831b6dc93ef3552c51be72cdf8ed1e7
matisuarezm <mati.suarez@duocuc.cl>
2025-09-06 10:39:34 -0400

    login V2

--- login.java
+++ login.java
@@ -5,6 +5,9 @@
 import java.util.*;
 public class hellogit {
     public static void main(String[] args) {
+
+        System.out.println("Login V2!");
+
         System.out.println("Ingresa usuario y contraseña");
 
         Scanner input = new scanner(System.in);
