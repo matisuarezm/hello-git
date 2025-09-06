7095ec484ede423c016492f42ecad7ed6162d7d5
matisuarezm <mati.suarez@duocuc.cl>
2025-09-05 21:50:09 -0400

    Agrega hellogit3 clase scanner

---
+++ hellogit3.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author msuarez
+ */
+import java.util.*;
+public class hellogit {
+    public static void main(String[] args) {
+        System.out.println("Holla GitHub");
+
+        Scanner input = new scanner(System.in);
+
+
+    }
+}
