c56cb7a11b06570640d4fbfe2f037cc2f32a386c
matisuarezm <mati.suarez@duocuc.cl>
2025-09-05 20:22:11 -0400

    Cambio en el print

--- hellogit.java
+++ hellogit.java
@@ -4,7 +4,7 @@
  */
 public class hellogit {
     public static void main(String[] args) {
-        System.out.println("Holla GitHub");        
+        System.out.println("New Hola GitHub");
         
     }    
 }
