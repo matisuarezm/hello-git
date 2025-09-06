a6913913509ada7edfafed5e6e16b2ec2fb9558a
matisuarezm <mati.suarez@duocuc.cl>
2025-09-05 22:06:00 -0400

    agregamos print user and name

---
+++ login.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author msuarez
+ */
+import java.util.*;
+public class hellogit {
+    public static void main(String[] args) {
+        System.out.println("Ingresa usuario y contraseña");
+
+        Scanner input = new scanner(System.in);
+
+
+    }
+}
