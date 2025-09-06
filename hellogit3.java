d338f42c970198826eb71b074a34fde0ecc0653c
matisuarezm <mati.suarez@duocuc.cl>
2025-09-06 10:07:36 -0400

    v5 full hellogit3.java

--- hellogit3.java
+++ hellogit3.java
@@ -5,7 +5,7 @@
 import java.util.*;
 public class hellogit {
     public static void main(String[] args) {
-        System.out.println("Holla GitHub V3");
+        System.out.println("Holla GitHub V5! full");
 
         Scanner input = new scanner(System.in);
 
